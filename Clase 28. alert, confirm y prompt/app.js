e7f48224e85a87f50dbe464d8b678d12a7a6ef35
AndresGarcia <andresgarcia@worldofficeonline.com>
2021-09-13 12:22:16 -0500

    Se agrega código clase 28

---
+++ Clase 28. alert, confirm y prompt/app.js
@@ -0,0 +1,17 @@
+// alert, confirm y prompt
+
+console.log(window);
+// window.alert("Hola esto es una alerta");
+// window.confirm("Hola esto es una confirmación");
+// window.prompt("Hola esto es un prompt");
+
+// alert("Hola esto es una alerta");
+// confirm("Hola esto es una confirmación");
+// prompt("Hola esto es un prompt");
+
+alert("Hola esto es una alerta");
+let confirmacion = confirm("Hola esto es una confirmación"),
+    aviso = prompt("Hola esto es un prompt (aviso)");
+
+console.log(confirmacion);
+console.log(aviso);
