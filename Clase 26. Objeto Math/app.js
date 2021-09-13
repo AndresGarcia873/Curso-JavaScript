6246ea4bb7f3331ba73e9ad8d0bda8b77a5d7bab
AndresGarcia <andresgarcia@worldofficeonline.com>
2021-09-13 11:26:54 -0500

    Se agrega código clase 26

---
+++ Clase 26. Objeto Math/app.js
@@ -0,0 +1,29 @@
+// Objeto Math
+
+console.log(Math);
+console.log(Math.PI);
+
+// Devuelve número absoluto, es decir su valor númerico sin signo 
+console.log(Math.abs(-7.8));
+
+// Redondear número siempre hacia arriba
+console.log(Math.ceil(7.2));
+
+// Redondear número siempre hacia abajo
+console.log(Math.floor(7.8))
+
+// Redondear dependiendo el decimal
+console.log(Math.round(7.49));
+
+// Raiz cuadrada
+console.log(Math.sqrt(81));
+
+// Elevar potencias
+console.log(Math.pow(2, 5));
+
+// Si el valor es positivo = 1, si es negativo = -1 y si es 0 = 0 
+console.log(Math.sign(-7.8));
+
+// Número random
+console.log(Math.random());
+console.log(Math.round(Math.random() * 1000));
